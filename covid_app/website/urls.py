2840649b095a7798af851b41fdebaf091e70792a
Frederico Ribeiro <fredericobsr@gmail.com>
2020-07-16 15:57:49 -0300

    Created navbar

--- covid_app/website/urls.py
+++ covid_app/website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path(r'', RedirectView.as_view(url='/sintomas/')),
     path(r'sintomas/', views.SintomasView.as_view(), name='sintomas'),
     path(r'recomendacoes/', views.RecomendacoesView.as_view(), name='recomendacoes'),
+    path(r'graficos/', views.GraficosView.as_view(), name='graficos'),
     path(r'mitos/', views.MitosView.as_view(), name='mitos'),
     path(r'faq/', views.FaqView.as_view(), name='faq'),
 ]
